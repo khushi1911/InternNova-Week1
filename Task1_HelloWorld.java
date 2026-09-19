609314873ab8df93710b1a725ef0a03f5b1a593e
khushi1911 <khushikaushik2030@gmail.com>
2026-09-19 17:24:44 +0530

    Add files via upload
    task1

---
+++ Task1_HelloWorld.java
@@ -0,0 +1,11 @@
+public class Task1_HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name: Khushi Kumari");
+        System.out.println("College: Dayananda Sagar University");
+        System.out.println("Branch: B.Tech (CSE - Data Science)");
+        System.out.println("Welcome to Java Programming!");
+
+    }
+}
