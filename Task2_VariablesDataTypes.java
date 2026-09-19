022314425a3fb2a9cd43306b8e871844f56fd639
khushi1911 <khushikaushik2030@gmail.com>
2026-09-19 17:25:10 +0530

    Add files via upload
    task 2

---
+++ Task2_VariablesDataTypes.java
@@ -0,0 +1,21 @@
+public class Task2_VariablesDataTypes {
+
+    public static void main(String[] args) {
+
+        String name = "Khushi Kumari";
+        int age = 19;
+        double height = 159.0;
+        double weight = 45.0;
+        double cgpa = 9.66;
+        char gender = 'F';
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " cm");
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Gender: " + gender);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
